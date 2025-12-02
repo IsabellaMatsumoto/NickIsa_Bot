70013065a4a34a31fe2d7b01d641e435dadbfbcd
Isabella Matsumoto <149169865+IsabellaMatsumoto@users.noreply.github.com>
2025-12-02 12:22:40 -0300

    Update API.py

--- API.py
+++ API.py
@@ -1,24 +1,20 @@
+#Código de teste
 import requests
 import json
 
-# === SEU TOKEN DA API ===
 FOOTBALL_TOKEN = ""
 
-# === URL da API da tabela do Brasileirão ===
 url = "https://api.football-data.org/v4/competitions/BSA/matches"
 
-# === Cabeçalho com o token (obrigatório) ===
 headers = {
     "X-Auth-Token": FOOTBALL_TOKEN
 }
 
-# === Faz a requisição ===
 response = requests.get(url, headers=headers)
 
-# === Verifica se deu certo ===
 if response.status_code == 200:
     data = response.json()
-    # Mostra o JSON completo formatado (como no navegador)
+
     print(json.dumps(data, indent=4, ensure_ascii=False))
 else:
     print(f"❌ Erro {response.status_code}: {response.text}")
